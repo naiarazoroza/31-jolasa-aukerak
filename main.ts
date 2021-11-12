1badf8001335621af3e3708e45f86a1e2db21599
naiarazoroza <93262292+naiarazoroza@users.noreply.github.com>
2021-11-12 14:28:35 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    puntua.move(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    puntua.move(1)
+})
+input.onButtonPressed(Button.B, function () {
+    puntua.move(1)
+})
+let puntua: game.LedSprite = null
+puntua = game.createSprite(2, 2)
