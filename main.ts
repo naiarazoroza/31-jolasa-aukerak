043a6eb108b40a9b81ec97f869a33519b09aa9c4
naiarazoroza <93262292+naiarazoroza@users.noreply.github.com>
2021-11-17 14:06:35 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    puntua.move(1)
+    puntua.move(-1)
 })
 input.onButtonPressed(Button.AB, function () {
-    puntua.move(1)
+    puntua.change(LedSpriteProperty.Y, 1)
 })
 input.onButtonPressed(Button.B, function () {
     puntua.move(1)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    puntua.change(LedSpriteProperty.Y, -1)
 })
 let puntua: game.LedSprite = null
 puntua = game.createSprite(2, 2)
